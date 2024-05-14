e31f7fe62a284119e2fb2b4a88ce27a2fbdd7c7b
Evgenii Dudnik <johnydvgu@gmail.com>
2024-05-14 13:01:41 +0400

    static part is done

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,22 @@
+import StickyWrapper from "@/components/sticky-wrapper";
+import FeedWrapper from "@/components/feed-wrapper";
+import Header from "@/app/(main)/learn/header";
+import UserProgress from "@/components/user-progress";
+
 const LearnPage = () => {
   return (
-    <div>
-      Learn
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress 
+          activeCourse={{ title: 'Spanish', imgSrc: '/es.svg' }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
     </div>
   );
 };
